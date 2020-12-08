d3a6597cd0e19d75ac2d43d7f3fd93efa0762b58
Shiv <siva9kolli@gmail.com>
2020-12-08 06:02:14 +0530

    docker execution ok

--- src/main/java/org/mac/HerokuInternetSampleFramework/BaseTest.java
+++ src/main/java/org/mac/HerokuInternetSampleFramework/BaseTest.java
@@ -1,7 +1,14 @@
 package org.mac.HerokuInternetSampleFramework;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.*;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -11,9 +18,13 @@ public class BaseTest {
 	public WebDriver driver;
 	
 	@BeforeSuite
-	public void lauchBrowser() {
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
+	public void lauchBrowser() throws MalformedURLException{
+
+		ChromeOptions options = new ChromeOptions();
+		options.setCapability("platform", Platform.LINUX);
+		
+		
+		driver = new RemoteWebDriver(new URL("http://192.168.1.4:4444/wd/hub"), options);
 		driver.get("http://the-internet.herokuapp.com/");
 	}
 	
